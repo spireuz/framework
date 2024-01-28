a71a5d5031b950cffec17f65848aba7bddbc864e
Khurshidbek Kobilov <kabilovhurshid5@gmail.com>
2024-01-28 05:27:13 +0500

    add slim router and psr-7 message implementation

--- app/index.php
+++ app/index.php
@@ -1,5 +1,15 @@
 <?php
 
-require_once __DIR__ . '/../src/bootstrap.php';
+use Slim\Psr7\Request;
+use Slim\Psr7\Response;
 
-echo "Hello from Spire Framework";
+require_once __DIR__ . '/../vendor/autoload.php';
+
+$app = require_once  __DIR__ . '/../src/app.php';
+
+$app->get('/', function (Request $request, Response $response) {
+    $response->getBody()->write('<h1>Hello from Spire Framework</h1>');
+    return $response;
+});
+
+$app->run();
---
+++ src/app.php
@@ -0,0 +1,8 @@
+<?php
+
+use Slim\Factory\AppFactory;
+
+$app = AppFactory::create();
+
+return $app;
+
